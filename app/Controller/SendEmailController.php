b7521344fd82e6c37f540c5e47bc04808526797c
Manuel Correa <manuelc123@gmail.com>
2020-09-25 07:41:33 -0500

    recuperar ajustes realziados

--- app/Controller/SendEmailController.php
+++ app/Controller/SendEmailController.php
@@ -17,7 +17,7 @@ class SendEmailController extends Controller
 
 
 
-        $url="https://www.gatosrugby.com.co/GRC/";
+        $url="http://www.gatosrugby.com.co/GRC/";
         $imagen = "https://scontent.feoh4-2.fna.fbcdn.net/v/t1.0-9/81006602_2604729209574305_6698650403245916160_n.png?_nc_cat=111&_nc_sid=8024bb&_nc_eui2=AeE7_utBh40_NDEeyrdfun2vo15f7nWKQ_ujXl_udYpD--AwGvnYqE9JgnC9pzgyonSW2XFrrSTeagITiohLl5aL&_nc_ohc=76gYGgOJP8sAX_mZ0JJ&_nc_ht=scontent.feoh4-2.fna&oh=3438ab55351a300f6edb59e85407413a&oe=5EC73B0A";
         $asunto = "Felicitaciones te has registrado correctamente";
         
@@ -65,7 +65,7 @@ public function sendEmailCambioClave($request,$response,$args){
 
 
 
-        $url="https://www.gatosrugby.com.co/GRC/";
+        $url="http://www.gatosrugby.com.co/GRC/";
         $imagen = "https://scontent.feoh4-2.fna.fbcdn.net/v/t1.0-9/81006602_2604729209574305_6698650403245916160_n.png?_nc_cat=111&_nc_sid=8024bb&_nc_eui2=AeE7_utBh40_NDEeyrdfun2vo15f7nWKQ_ujXl_udYpD--AwGvnYqE9JgnC9pzgyonSW2XFrrSTeagITiohLl5aL&_nc_ohc=76gYGgOJP8sAX_mZ0JJ&_nc_ht=scontent.feoh4-2.fna&oh=3438ab55351a300f6edb59e85407413a&oe=5EC73B0A";
         $asunto = "Felicitaciones tu clave se ha actualizado correctamente";
         
